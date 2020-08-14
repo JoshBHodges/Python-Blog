f777272646ca6e547df7ac71c353c858c14a2dc8
Joshua Hodges <j.b.h.0411@gmail.com>
2020-08-14 16:34:22 +0100

    Finished the Auth and the Blog CRUD functions

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,7 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -14,17 +15,19 @@ def get_db():
 
     return g.db
 
+
 def close_db(e=None):
-    db = g.pop('db',None)
+    db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
 
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.execute(f.read().decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
@@ -33,6 +36,7 @@ def init_db_command():
     "Clear the existing data and create new tables."
     init_db()
     click.echo('Initialise Database.')
+
 
 def init_app(app):
     app.teardown_appcontext(close_db)
